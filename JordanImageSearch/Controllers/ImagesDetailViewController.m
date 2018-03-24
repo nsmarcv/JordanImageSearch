eb25d170b6fb9804012b1f48dd6cad5f886871c9
Jordan Lepretre <JordanLepretre.JL@gmail.com>
2018-03-25 00:09:03 +0100

    add fall animation

--- JordanImageSearch/Controllers/ImagesDetailViewController.m
+++ JordanImageSearch/Controllers/ImagesDetailViewController.m
@@ -9,11 +9,18 @@
 #import "ImagesDetailViewController.h"
 #import "UIImageView+WebCache.h"
 #import "UIView+WebCache.h"
+#import "UIColor+Utilities.h"
 
 @interface ImagesDetailViewController ()
 
 @property (strong, nonatomic) IBOutlet UIScrollView *imagesScrollView;
 @property (strong, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (strong, nonatomic) IBOutlet UIView *pageControlBackground;
+
+
+@property (strong, nonatomic) IBOutlet UIButton *fallButton;
+@property (strong, nonatomic) IBOutlet UILabel *hireMeLabel;
+
 
 @end
 
@@ -22,8 +29,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self initImagesScrollView];
+}
+
+
+- (void) initImagesScrollView {
+    //Remove all subviews
+    [[_imagesScrollView subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
     if([_imagesList count] > 0){
-        
         CGFloat screenWidth = ([[UIScreen mainScreen] bounds].size.width);
         
         _pageControl.numberOfPages = [_imagesList count];
@@ -56,6 +70,63 @@
     
     //Calculate the page we are on based on x coordinate position and width of scroll view
     _pageControl.currentPage = (int)xPos/width;
+}
+
+//Fall Animation
+- (IBAction)fallButton:(id)sender {
+    if([_fallButton.titleLabel.text isEqualToString:@"DO NOT PRESS"]){
+        [_pageControl setHidden:YES];
+        [_pageControlBackground setHidden:YES];
+        [_hireMeLabel setHidden:NO];
+        
+        [_fallButton setEnabled:NO];
+        
+        CGFloat screenWidth = ([[UIScreen mainScreen] bounds].size.width);
+        CGFloat screenHeight = ([[UIScreen mainScreen] bounds].size.height);
+        
+        CGRect newFrame = CGRectMake(_imagesScrollView.contentOffset.x+screenWidth/2,
+                                     screenHeight,
+                                     screenWidth/6,
+                                     screenWidth/6);
+        
+        for(UIView *view in [_imagesScrollView subviews]){
+            if([view isKindOfClass:[UIImageView class]]){
+                
+                [UIView animateWithDuration:0.50 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+                    //If view is on the left (from visible view), rotate right
+                    if(view.frame.origin.x < _imagesScrollView.contentOffset.x){
+                        [view setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
+                    }
+                    //Else if view is on the right (from visible view), rotate left
+                    else if(view.frame.origin.x > _imagesScrollView.contentOffset.x){
+                        [view setTransform:CGAffineTransformMakeRotation(-M_PI / 2)];
+                    }
+                    //Else if view is in center, no rotation but send view to back
+                    else{
+                        [_imagesScrollView sendSubviewToBack:view];
+                    }
+                    
+                    //Change frame to move it down
+                    view.frame = newFrame;
+                    
+                } completion:^(BOOL finished) {
+                    [_fallButton setEnabled:YES];
+                    [_fallButton setTitle:@"REMETTRE" forState:UIControlStateNormal];
+                    _fallButton.backgroundColor = [UIColor colorFromHexString:@"#68D89B"];
+                }];
+            }
+        }
+    }
+    else{
+        [_pageControl setHidden:NO];
+        [_pageControlBackground setHidden:NO];
+        [_hireMeLabel setHidden:YES];
+        
+        [_fallButton setTitle:@"DO NOT PRESS" forState:UIControlStateNormal];
+        _fallButton.backgroundColor = [UIColor colorFromHexString:@"#FF7675"];
+        
+        [self initImagesScrollView];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
